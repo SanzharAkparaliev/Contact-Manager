e72ab6c851f88427aab7f2504e9f8905930a2ec8
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-04-20 00:07:14 +0600

    first commit

---
+++ src/main/java/com/contact/manager/ContactManager.java
@@ -0,0 +1,13 @@
+package com.contact.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManager {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactManager.class, args);
+	}
+
+}
