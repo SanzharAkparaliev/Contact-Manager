487061bcc963c446a85aadded653c526c1186e18
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-04-20 15:37:43 +0600

    setting bootstrap || templates

--- src/main/java/com/contact/manager/contrller/HomeController.java
+++ src/main/java/com/contact/manager/contrller/HomeController.java
@@ -5,23 +5,18 @@ import com.contact.manager.entity.User;
 import com.contact.manager.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@ResponseBody
 public class HomeController {
     @Autowired
     private UserRepository userRepository;
-    @GetMapping("/test")
-    public String test(){
-        User user = new User();
-        user.setName("Sanzhar");
-        user.setEmail("sanzhar@gmail.com");
-        Contact contact = new Contact();
-        user.getContactList().add(contact);
 
-        userRepository.save(user);
-        return "Working";
+    @GetMapping("/home")
+    public String test(Model model){
+        model.addAttribute("title","Home - Smart Contact Manager");
+        return "home";
     }
 }
