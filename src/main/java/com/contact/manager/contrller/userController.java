85a11ef9d6b26543c7f4dd081cdc3de7e5b3c960
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-04-22 22:42:58 +0600

    add login system

---
+++ src/main/java/com/contact/manager/contrller/userController.java
@@ -0,0 +1,14 @@
+package com.contact.manager.contrller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class userController {
+    @GetMapping("/index")
+    public String dashboard(){
+        return "normal/user_dashboard";
+    }
+}
