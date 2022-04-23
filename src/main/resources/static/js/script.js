ab8f0d51a1411af10adbcdb08953c21d9cc8b437
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-04-23 22:59:58 +0600

    creating responsive sidebar

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,10 @@
 console.log("this is script")
+const toggleSidebar = () => {
+    if($(".sidebar").is(":visible")){
+        $(".sidebar").css("display","none");
+        $(".content").css("margin-left","0%")
+    }else{
+        $(".sidebar").css("display","block");
+        $(".content").css("margin-left","20%")
+    }
+}
